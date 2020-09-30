7deaf028168157613cb8f6f88cb22565e78ade0f
BuildTools <unconfigured@null.spigotmc.org>
2020-09-30 16:08:59 +0000

    add welcome message

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 const mineflayer = require('mineflayer');
 const mineflayerViewer = require('prismarine-viewer').mineflayer;
 const prompt = require('prompt-sync')();
+const chalk = require("chalk");
+const boxen = require("boxen");
 
 //const user = prompt('\033[0;36mEmail: ');
 //const pass = prompt('\033[0;36mPass: ');
 //const host = prompt('\033[0;36mServer: ');
+
+const greeting = chalk.white.bold("   Minecraft    \nTerminal Edition");
+
+const boxenOptions = {
+ padding: 0,
+ margin: 1,
+ borderColor: "green",
+ backgroundColor: "gray"
+};
+const msgBox = boxen( greeting, boxenOptions );
+
+console.log(msgBox);
 
 const bot = mineflayer.createBot({
   username: `cheeto`,
