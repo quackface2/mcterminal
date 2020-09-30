c9abb3d89fe43a662a8b6d316c33a80e0b4c72ca
BuildTools <unconfigured@null.spigotmc.org>
2020-09-30 14:46:30 +0000

    ask user for login

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const mineflayer = require('mineflayer');
 const mineflayerViewer = require('prismarine-viewer').mineflayer;
+const prompt = require('prompt-sync')();
+const prompt = require('prompt-sync')({sigint: true});
+
+const user = prompt('Email: ');
+const pass = prompt('Pass: ');
+const server = prompt('Server: ');
 
 const bot = mineflayer.createBot({
-  username: 'User',
+  username: `${user}`,
+  password: `${pass}`,
+  server: `${server}`,
   version: false
 })
 
